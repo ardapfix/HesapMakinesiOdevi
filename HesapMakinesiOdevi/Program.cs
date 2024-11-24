5fa3fd34be0c82a8bb735bb516033408c32f965b
ardaa <ardaa@DESKTOP-3JG0MAF>
2024-11-24 13:32:17 +0300

    Proje dosyası ekle.

---
+++ HesapMakinesiOdevi/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HesapMakinesiOdevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ilksayi;
+            int ikincisayi;
+            int sonuc;
+            int fark;
+            int toplam;
+            int bolum;
+            int carpim;
+
+            Console.WriteLine("Hesap Makinesi Ödevi");
+            Console.WriteLine("----------");
+            Console.WriteLine();
+
+            
+            Console.WriteLine("İlk Sayıyı Giriniz : ");
+            ilksayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci Sayıyı Giriniz : ");
+            ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+            toplam = ilksayi + ikincisayi;
+            fark = ilksayi - ikincisayi;
+            bolum = ilksayi / ikincisayi;
+            carpim = ilksayi * ikincisayi;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Girilen 2 Sayının : ");
+            Console.WriteLine("----------");
+            Console.WriteLine("Toplamı : " + toplam);
+            Console.WriteLine("Farkı : " + fark);
+            Console.WriteLine("Bölümü : " + bolum);
+            Console.WriteLine("Çarpımı : " + carpim);
+            Console.WriteLine("----------");
+
+            Console.ReadKey();
+        }
+    }
+}
